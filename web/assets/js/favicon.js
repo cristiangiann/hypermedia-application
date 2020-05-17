fd52718b31b398b423f59596fcc2629d0a1a70c0
Stefano Formicola <stefano_17sf@libero.it>
2020-05-17 11:12:12 +0200

    favicon added

---
+++ web/assets/js/favicon.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("head").append("<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/assets/favicon_io/apple-touch-icon.png\"\>");
+    $("head").append("<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/assets/favicon_io/favicon-32x32.png\">");
+    $("head").append("<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/assets/favicon_io/favicon-16x16.png\">");
+    $("head").append("<link rel=\"manifest\" href=\"/assets/favicon_io/site.webmanifest\">");
+})
