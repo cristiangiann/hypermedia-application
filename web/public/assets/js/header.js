d42f7ba073b2fbb781a446457c6d1aa32cf1ed35
Stefano Formicola <stefano_17sf@libero.it>
2020-05-18 15:25:07 +0200

    nav-items set as active in navbar

--- web/public/assets/js/header.js
+++ web/public/assets/js/header.js
@@ -1,9 +1,20 @@
-$(document).ready(function () {
-    $.get("/pages/header.html", function (data) {
+let addHeader = function(completionHandler = function() {}) {
+    $.get("../../pages/header.html", function (data) {
         $("#page-content").prepend("<!-- Content -->\n");
         $("#page-content").prepend(data);
         $("#page-content").prepend("<!-- Header section-->");
         $("#page-content").prepend("");
         $("#page-content").append("<div id=\"push\"></div>");
+        completionHandler();
     }, "html");
+};
+
+let activateNavItem = function() {
+    let pageName = $("meta[name='keywords']").attr("content");
+    let navItem = $("#"+pageName);
+    navItem.addClass("active");
+}
+
+$(document).ready(function() {
+    addHeader(activateNavItem);
 });
