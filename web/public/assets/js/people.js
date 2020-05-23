78d4aef4592d9019bee2a5af08513f5f2d5c7959
Stefano Formicola <stefano_17sf@libero.it>
2020-05-24 00:42:22 +0200

    removed useless div class (people-item)

--- web/public/assets/js/people.js
+++ web/public/assets/js/people.js
@@ -13,7 +13,7 @@ function fetchPeople() {
 
             let htmlString = 
             '<a href="person?id=' + personId +'" class="col-lg-2 col-md-3 col-sm-4 col-5 px-auto mx-auto mx-sm-2 px-sm-2 mb-2">' +
-            '   <div class="people-item my-2 card border-0 bg-transparent">' +
+            '   <div class="my-2 card border-0 bg-transparent">' +
             '       <img src="' + personImgPath + '" class="card-img-top rounded-circle thumbnail" alt="person image">' +
             '       <div class="card-body p-0 text-center">' + 
             '           <h5 class="card-text">' + personNameSurname +'</h5>' +
