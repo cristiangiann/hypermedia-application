30a618b9d57337cae5d612b1af1417c93f850221
Stefano Formicola <stefano_17sf@libero.it>
2020-05-28 01:25:53 +0200

    events page refactored w/ cleaner JS

--- web/public/assets/js/events.js
+++ web/public/assets/js/events.js
@@ -23,23 +23,23 @@ function fetchEvents() {
 
 function drawNextEvents(filter) {
     let filteredArray = filterEventsByMonth(nextEvents, filter);
-    let htmlString = drawEvents(filteredArray);
-    $("#next-events-container").html(htmlString);
+    drawEvents(filteredArray, "#next-events-container");
 }
 
 function drawPastEvents(filter) {
     let filteredArray = filterEventsByMonth(pastEvents, filter);
-    let htmlString = drawEvents(filteredArray);
-    $("#past-events-container").html(htmlString);
+    drawEvents(filteredArray, "#past-events-container");
 }
 
 function filterEventsByMonth(events, month) {
     return (month === "all-months") ? events : events.filter(event => event.date.getMonth() == month-1);
 }
 
-function drawEvents(events) {
-    let htmlString = "";
+function drawEvents(events, containerName) {
+    let template = $("#event-item-template").html();
+    $(containerName).html("");
     events.forEach( event => {
+        let $eventItem = $(template);
         const eventURL = "/event?id=" + event.id.toString();
         const eventName = event.name;
         const eventDate = event.date;
@@ -51,31 +51,15 @@ function drawEvents(events) {
         const dateMonth = dateStrings[1];
         const dateYear = dateStrings[3];
         
-        htmlString +=                     
-                    '<!-- Event item -->' +
-                    '<div class="row mx-0 event-item align-items-center">' +
-                    '    <!-- Image -->' +
-                    '    <div class="col-md-2 order-2 order-md-1 px-3 px-sm-0">' +
-                    '        <img src="' + eventImgPath + '" alt="' + eventName + '" loading="lazy">' +
-                    '    </div>' +
-                    '    <!-- Title, date and description -->' +
-                    '    <div class="col-md-10 order-1 order-md-2 event-details pl-md-1">' +
-                    '        <div class="container-fluid">' +
-                    '            <div class="row mx-0 event-name text-center">' +
-                    '                <a href="' + eventURL + '"> <h3 class="text-center">' + eventName + '</h3> </a>' +
-                    '            </div>' +
-                    '            <div class="row mx-0 mt-lg-2">' +
-                    '                <div class="col-md-1 text-center order-1 px-0 align-self-center event-date">' +
-                    '                    <h3>' + dateDay + '</h3>' +
-                    '                    <h6>' + dateMonth + ' ' + dateYear + '</h6>' +
-                    '                </div>' +
-                    '                <div class="col-md-11 order-2 event-description">' + eventDescription + '</div>' +
-                    '            </div>' +
-                    '        </div>' +
-                    '    </div>' +
-                    '</div>';
+        $eventItem.find("img").attr("src", eventImgPath).attr("alt", eventName);
+        $eventItem.find("a").attr("href", eventURL);
+        $eventItem.find(".event-name h3").text(eventName);
+        $eventItem.find(".event-date > h3").text(dateDay);
+        $eventItem.find(".event-date > h6").text(dateMonth + " " + dateYear);
+        $eventItem.find(".event-description").html(eventDescription);
+
+        $(containerName).append($eventItem);
     });
-    return htmlString;
 }
 
 $(document).ready( () =>{
