3105ec8dde18e3bef470256343622b02abb5cc6e
Stefano Formicola <stefano_17sf@libero.it>
2020-05-28 00:57:40 +0200

    courses page refactored w/ cleaner JS

--- web/public/assets/js/courses.js
+++ web/public/assets/js/courses.js
@@ -6,27 +6,23 @@
 function fetchCourses() {
     // AJAX call
     $.get("/api/courses", (data) => {
-        let htmlString = "";
         data.forEach( course => {
             const courseURL = "/course?id=" + course.id.toString();
             const courseName = course.name;
             const courseImgPath = "../assets/imgs" + course.image_path;
 
-            htmlString += 
-            '<a href="' + courseURL + '"' + 'class="col-lg-2 col-md-3 col-sm-4 col-5 px-auto mx-auto mx-sm-2 px-sm-2 mb-2">' +
-            '   <div class="course-item my-2 card border-0 bg-transparent">' +
-            '       <img src="'+ courseImgPath +'" class="card-img-top" alt="' + courseName + '">' +
-            '       <div class="card-body p-0 text-center">' +
-            '           <h5 class="card-text">' + courseName + '</h5>' +
-            '       </div>' +
-            '   </div>' +
-            '</a>' +
-            '\n';
+            let template = $("#course-item-template").html();
+            let $courseItem = $(template);
+
+            $courseItem.attr("href", courseURL);
+            $courseItem.find("img").attr("src", courseImgPath);
+            $courseItem.find("img").attr("alt", courseName);
+            $courseItem.find("h5").text(courseName);
+            $("#courses-section").append($courseItem);
         });
-        $("#courses-section").append(htmlString);
     });
 }
 
 $(document).ready( () =>{
     fetchCourses();
-})
+});
