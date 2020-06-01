21b1a9fa7f979e7aa6e1d00329c929df3bbf88a2
Stefano Formicola <stefano_17sf@libero.it>
2020-06-01 15:57:41 +0200

    filtered menu entries in musical instruments page

--- web/public/assets/js/musical-instruments.js
+++ web/public/assets/js/musical-instruments.js
@@ -26,13 +26,12 @@ function fetchInstruments() {
 function drawInstruments(type, region) {
     let filteredArray = (type === allTypesValue) ? instruments : instruments.filter(instrument => (instrument.instrument_type_id == type));
         filteredArray = (region === allRegionsValue) ? filteredArray : filteredArray.filter(instrument => (instrument.italian_region_id == region)); 
-    console.log(filteredArray);
     let template = $("#instrument-item").html();
     $("#instruments-section").html(""); // clears the section, this is needed for filtering purposes
     filteredArray.forEach( instrument => {
         let $row = $(template);
         let $img = $row.find("img")
-        $row.attr("href", instrument.url);
+        $row.attr("href", "/musical-instrument?id="+instrument.id.toString());
         $row.find(".card-text").text(instrument.name);
         $img.attr("src", "../assets/imgs" + instrument.image_path);
         $img.attr("alt", instrument.name);
@@ -48,6 +47,7 @@ function fetchRegions() {
         // AJAX call
         $.get(getURL, (data) => {
             regions = data;
+            regions.unshift({"id": allRegionsValue, "name": "All regions"});
             sessionStorage.regions = JSON.stringify(regions);
             drawRegions();
         });
@@ -57,10 +57,11 @@ function fetchRegions() {
     }
 }
 
-function drawRegions() {
+function drawRegions(array) {
     let template = $("#select-rows").html();
-
-    regions.forEach( region => {
+    $("#regions-list").html("");
+    if (array == null) array = regions;
+    array.forEach( region => {
         let $row = $(template);
         $row.attr("value", region.id.toString());
         $row.text(region.name);
@@ -75,6 +76,7 @@ function fetchInstrumentTypes() {
         // AJAX call
         $.get(getURL, (data) => {
             instrumentTypes = data;
+            instrumentTypes.unshift({"id": allTypesValue, "name": "All types"});
             sessionStorage.instrumentTypes = JSON.stringify(instrumentTypes);
             drawInstrumentTypes();
         });
@@ -84,10 +86,11 @@ function fetchInstrumentTypes() {
     }
 }
 
-function drawInstrumentTypes() {
+function drawInstrumentTypes(array) {
     let template = $("#select-rows").html();
-
-    instrumentTypes.forEach( type => {
+    $("#types-list").html("");
+    if (array == null) array = instrumentTypes;
+    array.forEach( type => {
         let $row = $(template);
         $row.attr("value", type.id.toString());
         $row.text(type.name);
@@ -104,5 +107,35 @@ $(document).ready( () =>{
         const type = $("#types-list > option:selected").attr("value");
         const region = $("#regions-list > option:selected").attr("value");
         drawInstruments(type, region);
-    })
+    });
+
+    $("#types-list").change(function() {
+        const typeID = $("#types-list > option:selected").attr("value");
+        if (typeID == allTypesValue) $("#regions-list > option").show();
+        else {
+            $("#regions-list > option").each((_, region) => {
+                const elem = $(region);
+                const regionID = elem.attr("value");
+                if (regionID != allRegionsValue) {
+                    if (!instruments.some(i => (i.italian_region_id == regionID && i.instrument_type_id == typeID))) elem.hide();
+                    else elem.show();
+                }
+            });
+        }
+    });
+
+    $("#regions-list").change(function() {
+        const regionID = $("#regions-list > option:selected").attr("value");
+        if (regionID == allRegionsValue) $("#types-list > option").show();
+        else {
+            $("#types-list > option").each((_, type) => {
+                const elem = $(type);
+                const typeID = elem.attr("value");
+                if (typeID != allTypesValue) {
+                    if (!instruments.some(i => (i.italian_region_id == regionID && i.instrument_type_id == typeID))) elem.hide();
+                    else elem.show();
+                }
+            });
+        }
+    });
 })
