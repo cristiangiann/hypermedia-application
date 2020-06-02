4f48e706778bca1716254199442a304dc6722d4b
Stefano Formicola <stefanoformicola@icloud.com>
2020-06-02 22:29:16 +0200

    session storage for courses page

--- web/public/assets/js/courses.js
+++ web/public/assets/js/courses.js
@@ -1,24 +1,33 @@
 "use strict";
 
 
+let courses = sessionStorage.courses;
 
 // Fetching all courses from APIs
 function fetchCourses() {
-    // AJAX call
-    $.get("/api/courses", (data) => {
-        data.forEach( course => {
-            const courseName = course.name;
-            const courseImgPath = "../assets/imgs" + course.image_path;
-
-            let template = $("#course-item-template").html();
-            let $courseItem = $(template);
-
-            $courseItem.attr("href", course.url);
-            $courseItem.find("img").attr("src", courseImgPath);
-            $courseItem.find("img").attr("alt", courseName);
-            $courseItem.find(".card-text").text(courseName);
-            $("#courses-section").append($courseItem);
+    if (courses == null) {
+        // AJAX call
+        $.get("/api/courses", (data) => {
+            courses = data;
+            sessionStorage.courses = JSON.stringify(courses);
         });
+    }
+    drawCourses(courses);
+}
+
+function drawCourses(data) {
+    let template = $("#course-item-template").html();
+    data.forEach( course => {
+        const courseName = course.name;
+        const courseImgPath = "../assets/imgs" + course.image_path;
+
+        let $courseItem = $(template);
+
+        $courseItem.attr("href", course.url);
+        $courseItem.find("img").attr("src", courseImgPath);
+        $courseItem.find("img").attr("alt", courseName);
+        $courseItem.find(".card-text").text(courseName);
+        $("#courses-section").append($courseItem);
     });
 }
 
