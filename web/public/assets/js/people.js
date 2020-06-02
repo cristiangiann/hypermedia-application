511e47c335abe96a61c5b0aa1ebcea0d41f96ba0
Stefano Formicola <stefanoformicola@icloud.com>
2020-06-02 22:29:07 +0200

    session storage for people page

--- web/public/assets/js/people.js
+++ web/public/assets/js/people.js
@@ -1,23 +1,32 @@
 "use strict";
 
 
+let people = sessionStorage.people;
 
 // Fetching all people from APIs
 function fetchPeople() {
-    // AJAX call
-    $.get("/api/people", (data) => {
-        let template = $("#person-item-template").html();
-        data.forEach( person => {
-            let personNameSurname = person.name + " " + person.surname;
-            let personImgPath = "../assets/imgs" + person.image_path;
-            let personId = person.id;
-            let $personItem = $(template);
-            $personItem.attr("href", person.url);
-            $personItem.find("img").attr("src", personImgPath);
-            $personItem.find(".card-text").text(personNameSurname);
-            
-            $("#people-section").append($personItem);
+    if (people == null) {
+        // AJAX call
+        $.get("/api/people", (data) => {
+            people = data;
+            sessionStorage.people = JSON.stringify(people);
         });
+    }
+    drawPeople(people);
+}
+
+function drawPeople(data) {
+    let template = $("#person-item-template").html();
+    data.forEach( person => {
+        let personNameSurname = person.name + " " + person.surname;
+        let personImgPath = "../assets/imgs" + person.image_path;
+        let personId = person.id;
+        let $personItem = $(template);
+        $personItem.attr("href", person.url);
+        $personItem.find("img").attr("src", personImgPath);
+        $personItem.find(".card-text").text(personNameSurname);
+        
+        $("#people-section").append($personItem);
     });
 }
 
