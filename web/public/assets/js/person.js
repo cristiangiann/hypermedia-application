808f7806fc2ef19072833b9f049ec02bbe918911
Stefano Formicola <stefanoformicola@icloud.com>
2020-06-02 22:46:05 +0200

    people cached in session storage

--- web/public/assets/js/person.js
+++ web/public/assets/js/person.js
@@ -1,26 +1,37 @@
 "use strict";
 
 
+let peopleMap = sessionStorage.peopleMap;
+
 // Fetching a person from APIs
 function fetchPerson(id) {
     const getURL = "/api/people/"+id.toString();
-    // AJAX call
-    $.get(getURL, (person) => {
-        const personNameSurname = person.name + " " + person.surname;
-        const personImgPath = "../assets/imgs" + person.image_path;
-        const personTel = person.telephone;
-        const personEmail = person.email;
-        const personBiography = person.biography;
+    let person = peopleMap.get(id);
+    if (person == null) {
+        // AJAX call
+        $.get(getURL, (person) => {
+            peopleMap.set(id, person);
+            sessionStorage.peopleMap = peopleMap;
+        });
+    }
+    drawPerson(person)
+}
 
-        $("head > title").html(personNameSurname + " - Lemon Peel Association");
-        $("#person-name").text(personNameSurname);
-        $("#person-image").attr("src", personImgPath);
-        $("#person-image").attr("alt", personNameSurname);
-        $("#person-bio").html(personBiography);
-        $("#tel-number").text(personTel);
-        $("#email-addr").text(personEmail);
-        $("#courses-section").html(getCoursesHTML(person.courses));
-    });
+function drawPerson(person) {
+    const personNameSurname = person.name + " " + person.surname;
+    const personImgPath = "../assets/imgs" + person.image_path;
+    const personTel = person.telephone;
+    const personEmail = person.email;
+    const personBiography = person.biography;
+
+    $("head > title").html(personNameSurname + " - Lemon Peel Association");
+    $("#person-name").text(personNameSurname);
+    $("#person-image").attr("src", personImgPath);
+    $("#person-image").attr("alt", personNameSurname);
+    $("#person-bio").html(personBiography);
+    $("#tel-number").text(personTel);
+    $("#email-addr").text(personEmail);
+    $("#courses-section").html(getCoursesHTML(person.courses));
 }
 
 function getCoursesHTML(courses) {
@@ -39,6 +50,7 @@ function getCoursesHTML(courses) {
 }
 
 $(document).ready( () =>{
+    if (peopleMap == null) peopleMap = new Map();
     const params = new URLSearchParams(location.search);
     const key = 'id';
     if (params.has(key)) fetchPerson(params.get(key));
